ebd2e874fbf3d363c9a47e186ad2af4317c5395b
yasser-zm <zmproductions56@gmail.com>
2025-04-02 22:20:14 +0100

    Update calculator.asm

--- calculator.asm
+++ calculator.asm
@@ -3,6 +3,7 @@ DATA SEGMENT
     
     STR0 DB 'CE PROJET CALCULATRICE EST DEVELOPPE PAR : ZAMOUM YASSER ET MEGARI RAMZI $'
     NEWLINE DB 13, 10, '$'  
+    STR1  DB 'Entrez la base (Binaire, Decimale, ou Hexadecimale): $'
 DATA ENDS
 
 STACK SEGMENT
@@ -23,6 +24,10 @@ START:
     MOV DX, OFFSET NEWLINE ; print newline
     MOV AH, 09H
     INT 21H
+
+  MOV DX, OFFSET STR1   ; here it will load the address of str1 of the first element 
+    MOV AH, 09H           ; print the  string
+    INT 21H              
 
     MOV AH, 4CH   ; Exit program
     INT 21H
