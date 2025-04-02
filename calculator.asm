31afaf23b4ed3ed42b007e18641a20e2a9070432
yasser-zm <zmproductions56@gmail.com>
2025-04-02 21:52:52 +0100

    Create calculator.asm
    Added calculator.asm

---
+++ calculator.asm
@@ -0,0 +1,31 @@
+DATA SEGMENT 
+    
+    
+    STR0 DB 'CE PROJET CALCULATRICE EST DEVELOPPE PAR : ZAMOUM YASSER ET MEGARI RAMZI $'
+    NEWLINE DB 13, 10, '$'  
+DATA ENDS
+
+STACK SEGMENT
+    DW 128 DUP(?)  
+STACK ENDS
+
+CODE SEGMENT
+    ASSUME CS:CODE, DS:DATA, SS:STACK
+
+START:
+    MOV AX, DATA  
+    MOV DS, AX
+
+    MOV DX, OFFSET STR0   ; here it will load the address of str0 of the first element 
+    MOV AH, 09H           ; print the  string
+    INT 21H              
+
+    MOV DX, OFFSET NEWLINE ; print newline
+    MOV AH, 09H
+    INT 21H
+
+    MOV AH, 4CH   ; Exit program
+    INT 21H
+
+CODE ENDS
+END START
